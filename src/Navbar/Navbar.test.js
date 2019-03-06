c1d1ed96abbed62acb73f8c94aa6722d21ec489a
Austin <joha0033@gmail.com>
2019-03-06 11:41:29 -0700

    basic render test

--- src/Navbar/Navbar.test.js
+++ src/Navbar/Navbar.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home';
+import Navbar from './Navbar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
+  ReactDOM.render(<Navbar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
